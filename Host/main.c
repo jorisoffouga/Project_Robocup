7b36fe4577169c8ebb30759c5acc682a65e960b4
Joris Offouga <offougajoris@gmail.com>
2018-06-12 19:53:42 +0200

    add Host ressources

---
+++ Host/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>  // Standard input/output definitions
+#include <unistd.h> // UNIX standard function definitions
+#include <fcntl.h>  // File control definitions
+#include <errno.h>  // Error number definitions
+#include <string.h> // String function definitions
+#include <stdint.h> // Standard types
+#include <stdlib.h>
+#include "move.h"
+#include "../../Linux_Serial/serial.h"
+
+void display_help(const char *arg)
+{
+    fprintf(stderr, "Usage: %s \n", arg);
+    fprintf(stderr, "Example: %s /dev/ttyUSB0 9600\n", arg);
+}
+
+int main(int argc, char *agrv[])
+{
+    int i, fd = 0;
+    char buffer[100];
+    char *message="MPU6050 \r";
+
+    if (argc < 2)
+    {
+        display_help(agrv[0]);
+        return -1;
+    }
+
+    fd = open_serial(agrv[1], atoi(agrv[2]));
+
+    if (fd < 0)
+    {
+        return -1;
+    }
+
+    for (;;)
+    {
+
+        read_serial(fd, buffer, '\r', 10);
+        //printf("%s ", buffer);
+        //scanf("%10s", message);
+        write_serial(fd,message);
+        printf("%s",message);
+        fflush(stdout);
+    }
+
+    close_serial(fd);
+
+    return 0;
+}
---
+++ Host/move.h
@@ -0,0 +1,52 @@
+#ifndef MOVE_H_
+#define MOVE_H_
+
+
+#define Modulo 1000
+/*vector of motor 1 */
+#define vector_m1x (float)(-0.3655*Modulo)
+#define vector_m1y (float)(0.9371*Modulo)
+
+/*vector of motor 2 */
+#define vector_m2x (float)(-0.3685*Modulo)
+#define vector_m2y (float)(-0.9296*Modulo)
+
+/*vector of motor 3 */
+
+#define vector_m3x (float)(0.7051*Modulo)
+#define vector_m3y (float)(-0.7091*Modulo)
+
+/* vector of motor 4 */
+#define vector_m4x (float)(0.7074*Modulo)
+#define vector_m4y (float)(0.7068*Modulo) 
+
+typedef struct {
+	signed char x;
+	signed char y;
+	unsigned char speed_robot;
+}Direction;
+
+typedef struct{
+	float dmx;
+	float dmy;
+	float dmv;
+	unsigned short speed_motor;
+	unsigned char sens;
+}motor_type;
+
+typedef struct{
+	motor_type motor_1;
+	motor_type motor_2;
+	motor_type motor_3;
+	motor_type motor_4;
+	unsigned char speed_robot;
+}robot_type;
+
+void get_direction(robot_type *robot,signed char x, signed char y, unsigned char speed);
+void calcul_speed_motor(robot_type *robot);
+void move(robot_type *robot);
+
+
+
+
+#endif /* MOVE */
